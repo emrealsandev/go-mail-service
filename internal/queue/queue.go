e9ebe3e44407ad780367cd052cd4fc7cb3ab03a3
Emre ALŞAN <97472215+emrealsandev@users.noreply.github.com>
2023-12-15 23:36:00 +0300

    Last version (#7)
    * queueProcessor splitted and http response improved
    ---------
    
    Co-authored-by: emre.alsan <emre.alsan@flo.com.tr>

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -10,10 +10,10 @@ import (
 )
 
 type RequestStruct struct {
-	Email         string      `json:"email"`
-	TemplateAlias string      `json:"template_alias"`
-	SiteID        int         `json:"site_id"`
-	CustomData    interface{} `json:"custom_data"`
+	Email         string                 `json:"email"`
+	TemplateAlias string                 `json:"template_alias"`
+	SiteID        int                    `json:"site_id"`
+	CustomData    map[string]interface{} `json:"custom_data"`
 }
 
 func AddToQueue(toEmail string, templateAlias string, siteId int, customData map[string]interface{}, queueName string) error {
@@ -65,27 +65,9 @@ func handleQueueMessage(body []byte) {
 		return
 	}
 
-	// JSON formatındaki customData'yı map[string]interface{} türüne çözümle
-	var customData map[string]interface{}
-	switch v := mailRequest.CustomData.(type) {
-	case string:
-		// Zaten bir JSON stringi ise doğrudan çözümle
-		err := json.Unmarshal([]byte(v), &customData)
-		if err != nil {
-			log.Printf("CustomData çözümlenirken hata oluştu: %v", err)
-			return
-		}
-	case map[string]interface{}:
-		// Zaten bir harita ise doğrudan kullan
-		customData = v
-	default:
-		log.Printf("CustomData JSON formatında değil: %v", mailRequest.CustomData)
-		return
-	}
-
 	// Kuyruktan gelen her bir mesajı işle
-	// Mesajı mailProvider.go'daki SendMail fonksiyonuna yönlendir
-	err := mail.SendMail(mailRequest.Email, mailRequest.TemplateAlias, mailRequest.SiteID, customData)
+	log.Printf("İşlenen kayıt bilgileri: Email: %s, Template:%s, SiteId:%v, CustomData:%v", mailRequest.Email, mailRequest.TemplateAlias, mailRequest.SiteID, mailRequest.CustomData)
+	err := mail.SendMail(mailRequest.Email, mailRequest.TemplateAlias, mailRequest.SiteID, mailRequest.CustomData)
 	if err != nil {
 		log.Printf("E-posta gönderirken hata oluştu: %v", err)
 	}
